2ed4e6f07f2e5f625570dc7434066b01ddb16be1
DESKTOP-F79S1MD\Albert Yeoh <bertthings@gmail.com>
2018-09-12 00:14:45 +0800

    test

--- js/experiment-tracker.js
+++ js/experiment-tracker.js
@@ -49,7 +49,7 @@ class ExperimentTracker {
 
 	stopTimer() {
 		this.endTime = Date.now();
-		this.trials.push([this.trial, this.attempt, this.menuType, this.menuDepth, this.menuBreadth, this.isUsingMouse, this.targetItem, this.selectedItem, this.startTime, this.endTime, this.endTime - this.startTime]);
+		this.trials.push([this.trial, this.attempt, this.menuType, this.menuDepth, this.menuBreadth, this.isUsingMouse, this.targetItem, this.selectedItem, this.startTime, this.endTime, this.endTime - this.startTime, this.name, this.age, this.gender, this.education, this.masterhand, this.windowsOrMac, this.computerUsage, this.controlDevicePreference, this.softwares, this.experienceMarkingMenu, this.experienceRadialMenu, this.markingConfidence, this.radialConfidence, this.markingDifficulty, this.radialDifficulty, this.fasterOpinion, this.accurateOpinion]);
 		this.resetTimers();
 		this.attempt++;
 	}
@@ -59,7 +59,7 @@ class ExperimentTracker {
 	}
 
 	toCsv() {
-		var csvFile = "Trial,Attempt,Menu Type,Menu Depth, Menu Breadth, Using Mouse,Target Item,Selected Item,Start Time, End Time, Elapsed Time\n";
+		var csvFile = "Trial,Attempt,Menu Type,Menu Depth, Menu Breadth, Using Mouse,Target Item,Selected Item,Start Time, End Time, Elapsed Time, Name, Age, Gender, Education, Masterhand, Windows or Mac, Average Computer usage, Mouse or Trackpad Preference, Software usage, Marking Menu Experience, Radial Menu Experience, Marking Menu Confidence, Radial Menu Confidence, Marking Menu Difficulty, Radial Menu Difficulty, Faster Opinion, Accuracy Opinion\n";
 
 
 		for (var i = 0; i < this.trials.length; i++) {
